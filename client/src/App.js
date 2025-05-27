5e49a33b568fa954dc8787c1730bf49494463005
nishandutta <duttanishan12345@gmail.com>
2025-05-27 18:20:49 +0530

    background image

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import AddWord from './pages/AddWord'
-import './App.css'
 import EditWord from './pages/EditWord'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <div className='container mx-auto p-4'>
+    <div className='relative min-h-screen overflow-hidden'>
+      {/* Background from public folder */}
+      <div
+        className='absolute inset-0 bg-cover bg-center z-0 before:content-[""] before:absolute before:inset-0 before:bg-white/60 before:backdrop-blur-md'
+        style={{ backgroundImage: 'url(/background.jpg)' }}
+      ></div>
+
+      {/* Main content */}
+      <div className='relative z-10 container mx-auto p-4'>
         <h1 className='text-3xl font-bold text-center mb-6'>
           📘 Word Dictionary
         </h1>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/add' element={<AddWord />} />
-          <Route path='/edit/:id' element={<EditWord />} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/add' element={<AddWord />} />
+            <Route path='/edit/:id' element={<EditWord />} />
+          </Routes>
+        </Router>
       </div>
-    </Router>
+    </div>
   )
 }
 
